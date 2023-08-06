a4ffbdbf3ad257fb7eb4093de1e494c1b559c488
jgjoaogabrieldf <jgjoaogabrieldf@gmail.com>
2023-08-06 20:19:57 -0300

    first commit

---
+++ src/main/java/com/devsuperior/desafiodi/DesafiodiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiodiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiodiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafiodi/DesafiodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
